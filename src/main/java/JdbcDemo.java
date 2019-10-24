b11307871330585ed207b1f0a6852b3a0b7ad3ef
Elena Orlova <elenaorlovast@gmail.com>
2019-10-24 10:25:23 +0300

    try-with-resources added

--- src/main/java/JdbcDemo.java
+++ src/main/java/JdbcDemo.java
@@ -6,33 +6,29 @@ public class JdbcDemo {
     private static final String USER = "root";
     private static final String PASSWORD = "1122";
 
-    public static void main(String[] args) throws ClassNotFoundException, SQLException {
-        Connection connection = null;
-        Statement statement = null;
-
+    public static void main(String[] args) {
         System.out.println("Creating database connection...");
-        connection = DriverManager.getConnection(DATABASE_URL, USER, PASSWORD);
 
-        System.out.println("Executing statement...");
-        statement = connection.createStatement();
+        try (Connection connection = DriverManager.getConnection(DATABASE_URL, USER, PASSWORD);
+        Statement statement = connection.createStatement();) {
+            String query;
 
-        String query;
+            query = "SELECT * FROM teachers";
+            ResultSet resultSet = statement.executeQuery(query);
+            System.out.println("Retrieving data from database...");
+            while (resultSet.next()) {
+                String name = resultSet.getString("TeacherName");
+                String fName = resultSet.getString("TeacherFName");
+                String mName = resultSet.getString("TeacherMName");
 
-        query = "SELECT * FROM teachers";
-
-        ResultSet resultSet = statement.executeQuery(query);
-        System.out.println("Retrieving data from database...");
-        while (resultSet.next()) {
-            String name = resultSet.getString("TeacherName");
-            String fName = resultSet.getString("TeacherFName");
-            String mName = resultSet.getString("TeacherMName");
-
-            System.out.println("Teacher: " + fName + " " + name + " " + mName);
+                System.out.println("Teacher: " + fName + " " + name + " " + mName);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
         }
-
         System.out.println("Closing all connections...");
-        resultSet.close();
-        statement.close();
-        connection.close();
     }
 }
+
+
+
