77c14864f222834c6040beb8710d5106071fa89e
Elena Orlova <elenaorlovast@gmail.com>
2019-10-24 10:52:57 +0300

    getDataFromTeachersTable() added

--- src/main/java/JdbcDemo.java
+++ src/main/java/JdbcDemo.java
@@ -6,22 +6,33 @@ public class JdbcDemo {
     private static final String USER = "root";
     private static final String PASSWORD = "1122";
 
+    private static void getDataFromTeachersTable(Statement statement, String query) throws SQLException {
+        ResultSet resultSet = statement.executeQuery(query);
+        System.out.println("Retrieving data from database...");
+        while (resultSet.next()) {
+            String name = resultSet.getString("TeacherName");
+            String fName = resultSet.getString("TeacherFName");
+            String mName = resultSet.getString("TeacherMName");
+
+            System.out.println("Teacher: " + fName + " " + name + " " + mName);
+        }
+    }
+
     public static void main(String[] args) {
         System.out.println("Creating database connection...");
 
         try (Connection connection = DriverManager.getConnection(DATABASE_URL, USER, PASSWORD);
-        Statement statement = connection.createStatement();) {
+             Statement statement = connection.createStatement(
+                     ResultSet.TYPE_FORWARD_ONLY,
+                     ResultSet.CONCUR_UPDATABLE
+             )) {
             String query;
 
             query = "SELECT * FROM teachers";
-            ResultSet resultSet = statement.executeQuery(query);
-            System.out.println("Retrieving data from database...");
-            while (resultSet.next()) {
-                String name = resultSet.getString("TeacherName");
-                String fName = resultSet.getString("TeacherFName");
-                String mName = resultSet.getString("TeacherMName");
-
-                System.out.println("Teacher: " + fName + " " + name + " " + mName);
+            try {
+                getDataFromTeachersTable(statement, query);
+            } catch (SQLException e) {
+                e.printStackTrace();
             }
         } catch (SQLException e) {
             e.printStackTrace();
